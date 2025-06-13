6563daff5f6aeb0e77b12811128eaccdf54ca74a
P-ASilva <pedroas5.al.insper.edu>
2025-06-12 21:53:47 -0300

    feat: latest model

--- executable/main.py
+++ executable/main.py
@@ -1,17 +1,9 @@
-# main.py
-
 import sys
 from PyQt5.QtWidgets import QApplication
 from ui_main import MainWindow
-from select_region import select_screen_region
-from screen_capture import set_capture_region
 
 if __name__ == "__main__":
-    print("Please select the region where the video is playing...")
-    region = select_screen_region()
-    set_capture_region(region)
-
     app = QApplication(sys.argv)
-    window = MainWindow(region)
+    window = MainWindow()
     window.show()
     sys.exit(app.exec_())
